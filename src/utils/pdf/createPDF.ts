4fc1cd2bc40c94638fce4b4ee68d95e63253fd62
Mitch Rosenlund <mitch@Windows_Laptop>
2023-01-02 13:26:31 -0800

    Automate creating new planner pages

--- src/utils/pdf/createPDF.ts
+++ src/utils/pdf/createPDF.ts
@@ -14,43 +14,85 @@ import { WeeklyPreview } from "./weeklyPreview/WeeklyPreview";
 let pageCount = 1;
 
 async function createPdf(startDate: string, endDate: string) {
-  const days: [string, string, string][] = [
-    ["Monday", "12", "5"],
-    ["Tuesday", "12", "6"],
-    ["Wednesday", "12", "7"],
-    ["Thursday", "12", "8"],
-    ["Friday", "12", "9"],
-    ["Saturday", "12", "10"],
-    ["Sunday", "12", "11"],
-    ["Monday", "12", "12"],
-    ["Tuesday", "12", "13"],
-    ["Wednesday", "12", "14"],
-    ["Thursday", "12", "15"],
-    ["Friday", "12", "16"],
-    ["Saturday", "12", "17"],
-    ["Sunday", "12", "18"],
-    ["Monday", "12", "19"],
-    ["Tuesday", "12", "20"],
-    ["Wednesday", "12", "21"],
-    ["Thursday", "12", "22"],
-    ["Friday", "12", "23"],
-    ["Saturday", "12", "24"],
-    ["Sunday", "12", "25"],
-    ["Monday", "12", "26"],
-    ["Tuesday", "12", "27"],
-    ["Wednesday", "12", "28"],
-    ["Thursday", "12", "29"],
-    ["Friday", "12", "30"],
-    ["Saturday", "12", "31"],
-  ];
-  const pages = [DailyNotesPage(), WeeklyPreview("11", "20")];
-  for (const day of days) {
-    pages.push(DailyPage(day[0], day[1], day[2]));
-    pages.push(DailyNotesPage());
-    if (day[0] === "Sunday") {
-      pages.push(WeeklyPreview(day[1], day[2]));
+  const weekDays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ]
+
+  /**
+   * Notes on printing. Print even pages first. Then print odd pages by inserting the pages back in the printer after flipping them short wise.
+   * (Top of the printed page should be face up and closer to you)
+   */
+
+  const monthLength = 9
+
+  const month = "1"
+
+  let dayNum = 2
+
+  const pages = [DailyNotesPage(), WeeklyPreview("1", "1")];
+
+  while (dayNum <= monthLength) {
+    for (const day of weekDays) {
+      pages.push(DailyPage(day, month, `${dayNum}`));
+      pages.push(DailyNotesPage());
+      if (day === "Sunday") {
+        pages.push(WeeklyPreview(month, `${dayNum}`));
+      }
+      dayNum += 1;
+      if (dayNum > monthLength) {
+        break;
+      }
     }
   }
+
+  // const days: [string, string, string][] = [
+  //   ["Sunday", "1", "1"],
+  //   ["Monday", "1", "2"],
+  //   ["Tuesday", "1", "3"],
+  //   ["Thursday", "1", "4"],
+  //   ["Friday", "1", "5"],
+  //   ["Tuesday", "1", "6"],
+  //   ["Saturday", "1", "7"],
+  //   ["Sunday", "1", "8"],
+  //   ["Monday", "1", "9"],
+  //   ["Tuesday", "1", "10"],
+  //   ["Wednesday", "1", "11"],
+  //   ["Friday", "1", "12"],
+  //   ["Saturday", "1", "13"],
+  //   ["Sunday", "1", "14"],
+  //   ["Thursday", "1", "15"],
+  //   ["Friday", "1", "16"],
+  //   ["Saturday", "1", "17"],
+  //   ["Sunday", "1", "18"],
+  //   ["Monday", "1", "19"],
+  //   ["Tuesday", "1", "20"],
+  //   ["Wednesday", "1", "21"],
+  //   ["Thursday", "1", "22"],
+  //   ["Friday", "1", "23"],
+  //   ["Saturday", "1", "24"],
+  //   ["Sunday", "1", "25"],
+  //   ["Monday", "1", "26"],
+  //   ["Tuesday", "1", "27"],
+  //   ["Wednesday", "1", "28"],
+  //   ["Thursday", "1", "29"],
+  //   ["Friday", "1", "30"],
+  //   ["Saturday", "1", "31"],
+  // ];
+
+  // for (const day of days) {
+  //   pages.push(DailyPage(day[0], day[1], day[2]));
+  //   pages.push(DailyNotesPage());
+  //   if (day[0] === "Sunday") {
+  //     pages.push(WeeklyPreview(day[1], day[2]));
+  //   }
+  // }
+
   const docDefinition: TDocumentDefinitions = {
     pageSize: "A6",
     content: pages,
